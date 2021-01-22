8893a3994c409950f7e28153d7701282c3729624
Analogrithems <analogrithems@gmail.com>
2021-01-21 14:30:22 -1000

    added setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="reform",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(exclude=["docs", "tests"]),  # Required
     install_requires=["invoke", "pycryptodome", "pycryptodome", "pyee", "pathlib"],
     entry_points={"console_scripts": ["reform = reform:program.run"]},
