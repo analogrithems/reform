08fa4c0536b00297d09c8afe0005b73ec4801cc8
Aaron Collins <Aaron.Collinsa@gmail.com>
2021-04-09 18:41:18 -1000

    convert tp use poetry

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="reform",
-    version="0.1.10",
-    packages=find_packages(exclude=["docs", "tests"]),  # Required
-    install_requires=["invoke", "pycryptodome", "pycryptodome", "pyee", "pathlib"],
-    entry_points={"console_scripts": ["reform = reform:program.run"]},
-)
